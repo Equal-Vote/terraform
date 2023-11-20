2a2d0fe1593f54b1a43a0da664f6a9e615f789ce
Evans Tucker <gmail@evanstucker.com>
2023-11-19 21:13:59 -0800

    Azure VM instance naming is weird as hell

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   default_node_pool {
     name       = "agentpool"
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_B2als_v2"
     node_count = var.node_count
   }
 

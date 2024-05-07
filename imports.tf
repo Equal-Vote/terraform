0ba7cec80d9307ecabf58cf4b44dcd4a8f93eae7
Ata <ataahmad@Atas-MacBook-Pro.local>
2024-05-06 18:01:30 -0700

    Formats the imports file

--- imports.tf
+++ imports.tf
@@ -1,4 +1,4 @@
 import {
-    to = azurerm_key_vault.equalvote-argocd
-    id = "/subscriptions/86f3145a-48cc-4255-8757-dd3104d15e57/resourceGroups/equalvote/providers/Microsoft.KeyVault/vaults/equalvote-argocd/"
+  to = azurerm_key_vault.equalvote-argocd
+  id = "/subscriptions/86f3145a-48cc-4255-8757-dd3104d15e57/resourceGroups/equalvote/providers/Microsoft.KeyVault/vaults/equalvote-argocd/"
 }

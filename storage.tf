00ab7db7abd8035dbc69874343ae88f3de3f65b6
Arend Peter Castelein <apc1993@gmail.com>
2025-12-29 15:21:15 -0800

    Iterate on storage.tf

--- storage.tf
+++ storage.tf
@@ -1,32 +1,18 @@
-resource "azurerm_resource_group" "storage_rg" {
-  name     = "rg-default-storage"
-  location = "eastus"
-}
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "stdefaultstorageacct"
-  resource_group_name      = azurerm_resource_group.storage_rg.name
-  location                 = azurerm_resource_group.storage_rg.location
+  name                     = "defaultstorageacct"
+  resource_group_name      = azurerm_resource_group.equalvote.name
+  location                 = azurerm_resource_group.equalvote.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  allow_blob_public_access = false
+  account_kind             = "StorageV2"
+  access_tier              = "Hot"
   is_hns_enabled           = false
 }
 
 resource "azurerm_storage_container" "blobs" {
-  name                  = "default-container"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
+  name                 = "candidate-photos"
+  storage_account_id   = azurerm_storage_account.storage.id
+  container_access_type = "blob"
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.storage.name
-}
-
-output "storage_container_name" {
-  value = azurerm_storage_container.blobs.name
-}
-
-output "storage_resource_group" {
-  value = azurerm_resource_group.storage_rg.name
-}

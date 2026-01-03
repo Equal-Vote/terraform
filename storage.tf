764c52d24cbfb870283833a7a11c18b702e45a40
Arend Peter Castelein <apc1993@gmail.com>
2026-01-03 12:40:11 -0600

    Update storage

--- storage.tf
+++ storage.tf
@@ -4,14 +4,11 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name      = azurerm_resource_group.equalvote.name
   location                 = azurerm_resource_group.equalvote.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  access_tier              = "Hot"
-  is_hns_enabled           = false
+  account_replication_type = "LRS" # Locally redundant storage (the cheapest option)
 }
 
-resource "azurerm_storage_container" "blobs" {
-  name                 = "candidate-photos"
+resource "azurerm_storage_container" "candidate_photos" {
+  name                 = "candidate_photos"
   storage_account_id   = azurerm_storage_account.storage.id
   container_access_type = "blob"
 }

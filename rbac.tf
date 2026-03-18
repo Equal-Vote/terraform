ef14b896098b710936734dce4ffccf28fd36122c
3uzbcqje <3uzbcqje@addy.to>
2026-03-18 11:41:46 -0700

    Add Azure AD groups and Kubernetes RBAC for least-privilege access
    - Create Developer and DevOps Azure AD groups
    - Enable Kubernetes RBAC with Azure AD integration on AKS
    - Disable local accounts for improved security
    - Assign Azure Kubernetes Service Cluster User Role to both groups
    - Assign Azure Kubernetes Service RBAC Cluster Admin to DevOps group
    - Create ClusterRoleBindings for cluster-admin (DevOps) and view (Developer)

---
+++ rbac.tf
@@ -0,0 +1,38 @@
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.equalvote.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.equalvote.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.equalvote.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.equalvote.kube_config[0].cluster_ca_certificate)
+}
+
+resource "kubernetes_cluster_role_binding" "devops_cluster_admin" {
+  metadata {
+    name = "devops-cluster-admin"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "Group"
+    name      = azuread_group.devops.display_name
+    api_group = "azuread.io"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "developer_view" {
+  metadata {
+    name = "developer-view"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "view"
+  }
+  subject {
+    kind      = "Group"
+    name      = azuread_group.developer.display_name
+    api_group = "azuread.io"
+  }
+}
